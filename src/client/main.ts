103edd8890270523731502ef54321df1a5839984
TonyT <antony.trupe@gmail.com>
2024-08-21 19:29:29 -0400

    moved styles and removed some cruft

--- src/client/main.ts
+++ src/client/main.ts
@@ -23,9 +23,23 @@ const config: Phaser.Types.Core.GameConfig = {
 };
 
 const StartGame = (parent: string) => {
-
     return new Game({ ...config, parent });
-
 }
 
 export default StartGame;
+
+export const respondToVisibility = (element: HTMLElement | null, callback: any) => {
+    // console.log('element',element)
+    var options = {
+        root: null,
+    };
+
+    var observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            callback(entry.intersectionRatio > 0)
+        });
+    }, options)
+    if (!!element) {
+        observer.observe(element)
+    }
+}
